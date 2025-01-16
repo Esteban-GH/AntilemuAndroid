b52e6519359d3697e3b5739fc94ac48d4bbfa27d
Esteban <segundo.parra@virginiogomez.cl>
2025-01-16 15:29:51 -0300

    nose

--- app/src/main/java/com/example/antilemuapp/MainActivity.kt
+++ app/src/main/java/com/example/antilemuapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,16 +16,26 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var usuario : EditText = findViewById(R.id.txt_usuario)
+        var usu : EditText = findViewById(R.id.txt_usuario)
         var pass : EditText = findViewById(R.id.txt_pass)
         var ingresar : Button = findViewById(R.id.btn_ingresar)
 
         ingresar.setOnClickListener{
-            val intent = Intent(this, Admin::class.java)
-            startActivity(intent)
-            finish()
+            val usuario = usu.text.toString()
+            val contraseña = pass.text.toString()
+
+            if (usuario.isEmpty()) {
+                Toast.makeText(this, "Debe ingresar un Usuario", Toast.LENGTH_LONG).show()
+                }
+            else if(contraseña.isEmpty()) {
+                Toast.makeText(this, "Debe ingresar un Contraseña", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this, "Ingreso Correctamente", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, Admin::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
-
-
     }
 }
